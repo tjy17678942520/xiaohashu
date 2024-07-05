fd6468e03ddee0c0b10040a95ca1bd10d77818b7
Tu <tujiayuan5@gmail.com>
2024-07-05 13:54:02 +0800

    日志相关配置

--- xiaohashu-auth/src/test/java/com/quanxiaoha/xiaohashu/auth/XiaohashuAuthApplicationTests.java
+++ xiaohashu-auth/src/test/java/com/quanxiaoha/xiaohashu/auth/XiaohashuAuthApplicationTests.java
@@ -31,6 +31,20 @@ class XiaohashuAuthApplicationTests {
         userDOMapper.insert(userDO);
     }
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testLog() {
+        log.info("这是一行 Info 级别日志");
+        log.warn("这是一行 Warn 级别日志");
+        log.error("这是一行 Error 级别日志");
+
+        // 占位符
+        String author = "犬小哈";
+        log.info("这是一行带有占位符日志，作者：{}", author);
+    }
     /**
      * 删除数据
      */
