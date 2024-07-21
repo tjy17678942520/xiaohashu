72f266cee35bdd890f5d6942f9cf90b459e26a8c
Tu <tujiayuan5@gmail.com>
2024-07-21 09:54:10 +0800

    整合权限框架Sa-token框架

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
@@ -1,21 +1,14 @@
 package com.quanxiaoha.xiaohashu.auth.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.quanxiaoha.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.quanxiaoha.framework.common.response.Response;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
-/**
- * @author: 犬小哈
- * @url: www.quanxiaoha.com
- * @date: 2024/5/4 12:53
- * @description: TODO
- **/
+
 @RestController
 public class TestController {
 
@@ -30,4 +23,20 @@ public class TestController {
     public Response<User> test2(@RequestBody @Validated User user) {
         return Response.success(user);
     }
+
+    //测试登录
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        if ("zhan".equals(username)&&"123456".equals(password)){
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    @RequestMapping("/user/isLogin")
+    public String isLogin(){
+        return "当前登录状态："+StpUtil.isLogin();
+    }
+
 }
--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/User.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/User.java
@@ -8,12 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-/**
- * @author: 犬小哈
- * @url: www.quanxiaoha.com
- * @date: 2024/5/5 15:21
- * @description: TODO
- **/
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
